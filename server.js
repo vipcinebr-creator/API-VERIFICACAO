339ed1cabeb170d2324919b3acb71b2fff08e331
vipcinebr-creator <vipcinebr@gmail.com>
2026-01-17 20:00:25 -0300

    Fix formatting in server.js
    Fix formatting by adding a newline at the end of the file.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
